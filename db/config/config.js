70f13df38ddae32f5c52770bc2ba0551c089633b
Ante MBP <ntzurkovy@gmail.com>
2022-04-13 14:18:59 +0300

    Added husky pre-commit checks

--- db/config/config.js
+++ db/config/config.js
@@ -1,36 +1,35 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 require('dotenv').config();
-const parse = require('pg-connection-string').parse;
-const readFileSync = require('fs').readFileSync;
+import { parse } from 'pg-connection-string';
+import { readFileSync } from 'fs';
 
 const { user, password, host, database } = parse(process.env.DATABASE_URL);
 
-module.exports = { 
-  development: {
-    username: user,
-    password,
-    host,
-    database,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-        ca: readFileSync(`${__dirname}/../../global-bundle.pem`),
-      },
+export const development = {
+  username: user,
+  password,
+  host,
+  database,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+      ca: readFileSync(`${__dirname}/../../global-bundle.pem`),
     },
   },
-  production: {
-    username: user,
-    password,
-    host,
-    database,
-    dialect: 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        ca: readFileSync(`global-bundle.pem`),
-      },
+};
+export const production = {
+  username: user,
+  password,
+  host,
+  database,
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      ca: readFileSync(`global-bundle.pem`),
     },
-    ssl: true,
-  }
-}
+  },
+  ssl: true,
+};
