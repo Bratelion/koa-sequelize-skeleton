484c1c227ffbe2b2e031e258b902e7b9fd973e79
Ante MBP <ntzurkovy@gmail.com>
2022-05-04 12:31:15 +0300

    Updated config.js for sequelize

--- db/config/config.js
+++ db/config/config.js
@@ -1,35 +1,32 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require('dotenv').config();
-import { parse } from 'pg-connection-string';
-import { readFileSync } from 'fs';
+require('dotenv').config(); // eslint-disable-line @typescript-eslint/no-var-requires
+const parse = require('pg-connection-string').parse; // eslint-disable-line @typescript-eslint/no-var-requires
+const readFileSync = require('fs').readFileSync; // eslint-disable-line @typescript-eslint/no-var-requires
 
 const { user, password, host, database } = parse(process.env.DATABASE_URL);
 
-export const development = {
-  username: user,
-  password,
-  host,
-  database,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-      ca: readFileSync(`${__dirname}/../../global-bundle.pem`),
+module.exports = {
+  development: {
+    username: user,
+    password,
+    host,
+    database,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: false,
     },
   },
-};
-export const production = {
-  username: user,
-  password,
-  host,
-  database,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      ca: readFileSync(`global-bundle.pem`),
+  production: {
+    username: user,
+    password,
+    host,
+    database,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        ca: readFileSync(`global-bundle.pem`),
+      },
     },
+    ssl: true,
   },
-  ssl: true,
 };
